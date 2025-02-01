689272f902f71436c40a6fa28994e03ae456ae6f
thz22 <thjr5965@gmail.com>
2025-02-01 14:55:30 +0000

    SSA

--- kernel/sys.c
+++ kernel/sys.c
@@ -1204,28 +1204,6 @@ static int override_version(struct new_utsname __user *name)
 #endif
 }
 
-static int override_version(struct new_utsname __user *name)
-{
-#ifdef CONFIG_F2FS_REPORT_FAKE_KERNEL_VERSION
-	int ret;
-
-	if (strcmp(current->comm, "fsck.f2fs"))
-		return 0;
-
-	ret = copy_to_user(name->release, CONFIG_F2FS_FAKE_KERNEL_RELEASE,
-			   strlen(CONFIG_F2FS_FAKE_KERNEL_RELEASE) + 1);
-	if (ret)
-		return ret;
-
-	ret = copy_to_user(name->version, CONFIG_F2FS_FAKE_KERNEL_VERSION,
-			   strlen(CONFIG_F2FS_FAKE_KERNEL_VERSION) + 1);
-
-	return ret;
-#else
-	return 0;
-#endif
-}
-
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 extern void susfs_spoof_uname(struct new_utsname* tmp);
 #endif
